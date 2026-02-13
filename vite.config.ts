48c0e4a405f1e2c107792c75a16ee65f189ae81d
Ilyas <weendibol21300@gmail.com>
2026-02-13 13:09:47 +0500

    feat: set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/love-is/',
 	plugins: [vue(), vueJsx(), tailwindcss()],
 	resolve: {
 		alias: {
